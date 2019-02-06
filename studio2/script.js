44247e0a55403c87d1818efd44e7038077a3f741
anniely07 <annly@ucdavis.edu>
2019-02-06 09:00:58 -0800

    added css and images

--- studio2/script.js
+++ studio2/script.js
@@ -1,2 +1,40 @@
 'user strict'
 console.log('reading js');
+
+var woodstock = document.querySelector('#woodstock');
+var wsoverlay = document.querySelector('#wsoverlay');
+
+var pencil = document.querySelector('#pencil');
+var poverlay = document.querySelector('#poverlay')
+
+var kermit = document.querySelector('#kermit');
+var koverlay = document.querySelector('#koverlay')
+
+var wsclose = document.querySelector('#wsclose');
+var pclose = document.querySelector('#pclose');
+var kclose = document.querySelector('#kclose');
+
+
+woodstock.addEventListener('click',function(){
+  wsoverlay.style.display='block';
+});
+
+pencil.addEventListener('click', function(){
+  poverlay.style.display='block';
+});
+
+kermit.addEventListener('click', function(){
+  koverlay.style.display='block';
+});
+
+wsclose.addEventListener('click',function(){
+  wsoverlay.style.display='none';
+});
+
+pclose.addEventListener('click',function(){
+  poverlay.style.display='none';
+});
+
+kclose.addEventListener('click',function(){
+  koverlay.style.display='none';
+});

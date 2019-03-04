9da88662bb1b2d40d0426fcd8946aeba75977492
anniely07 <annly@ucdavis.edu>
2019-03-04 11:09:11 -0800

    added interactive wireframe

---
+++ process/development/interactive/script.js
@@ -0,0 +1,11 @@
+'user strict'
+console.log('reading js');
+
+var down = document.querySelector('#down');
+var topgone = document.querySelector('#one');
+
+down.addEventListener('click',function(){
+  zero.style.display='none';
+  one.style.display='block';
+  navigation.style.display='block';
+});
